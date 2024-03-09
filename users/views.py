53fbd19a628978fca37a797406e3e7f61b7cafb1
VLONE <ermolich158@gmail.com>
2024-03-09 16:24:54 +0300

    add login

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
 # Create your views here.
-
+@login_required
 def profile_view(request):
     return render(request, 'users/profile.html')
