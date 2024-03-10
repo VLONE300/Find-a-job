e50b0462435c600d820d07a9ada028f408939bad
VLONE <ermolich158@gmail.com>
2024-03-10 10:04:45 +0300

    add logout

--- find_job/settings.py
+++ find_job/settings.py
@@ -131,3 +131,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = reverse_lazy('users:profile')
+LOGOUT_REDIRECT_URL = reverse_lazy('users:profile')
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from users.views import profile_view
 
+app_name = 'users'
+
 urlpatterns = [
-    path('profile/', profile_view, name='profile')
+    path('profile/', profile_view, name='profile'),
+    path('logout/',LogoutView.as_view(), name='logout')
 ]
